bc4fee07d8d7c4e7a3b15bb1d5250c8a7ab4266c
Kevin Gilland <kgilland@caci.com>
2022-09-20 07:25:21 -0600

    chore: Port from develop branch.  New class GeometryConstants

--- lib/GeometryEnvelope.ts
+++ lib/GeometryEnvelope.ts
@@ -1,4 +1,4 @@
-import { Geometry, Point, LineString, Polygon } from "./internal";
+import { Geometry, Point, LineString, Polygon, Line } from "./internal";
 
 /**
  * Geometry envelope
@@ -60,30 +60,46 @@ export class GeometryEnvelope {
 	public constructor(minX: number, minY: number, maxX: number, maxY: number);
 	public constructor(minX: number, minY: number, minZ: number, maxX: number, maxY: number, maxZ: number);
 	public constructor(minX: number, minY: number, minZ: number, minM: number, maxX: number, maxY: number, maxZ: number, maxM: number);
-	public constructor(minX: number, minY: number, minZ: number, maxX: number, maxY: number, maxZ: number);
 
 	/**
 	 * Constructor
 	 */
-	constructor (...args) {
-		if (args.length === 1 && args[0] instanceof GeometryEnvelope) {this.minX = args[0].minX;
-		  this.maxX = args[0].maxX;
-		  this.minY = args[0].minY;
-		  this.maxY = args[0].maxY;
-		  this.hasZ = args[0].hasZ;
-		  this.minZ = args[0].minZ;
-		  this.maxZ = args[0].maxZ;
-		  this.hasM = args[0].hasM;
-		  this.minM = args[0].minM;
-		  this.maxM = args[0].maxM;
+	constructor(...args) {
+		if (args.length === 1 && args[0] instanceof GeometryEnvelope) {
+			this.minX = args[0].minX;
+			this.maxX = args[0].maxX;
+			this.minY = args[0].minY;
+			this.maxY = args[0].maxY;
+			this.hasZ = args[0].hasZ;
+			this.minZ = args[0].minZ;
+			this.maxZ = args[0].maxZ;
+			this.hasM = args[0].hasM;
+			this.minM = args[0].minM;
+			this.maxM = args[0].maxM;
 		} else if (args.length === 2) {
-
+			this.hasZ = args[0];
+			this.hasM = args[1];
 		} else if (args.length === 4) {
-
+			this.minX = args[0];
+			this.minY = args[1];
+			this.maxX = args[2];
+			this.maxY = args[3];
 		} else if (args.length === 6) {
-
+			this.minX = args[0];
+			this.minY = args[1];
+			this.minZ = args[2];
+			this.maxX = args[3];
+			this.maxY = args[4];
+			this.maxZ = args[5];
 		} else if (args.length === 8) {
-
+			this.minX = args[0];
+			this.minY = args[1];
+			this.minZ = args[2];
+			this.minM = args[3];
+			this.maxX = args[4];
+			this.maxY = args[5];
+			this.maxZ = args[6];
+			this.maxM = args[7];
 		}
 	}
 
@@ -93,7 +109,7 @@ export class GeometryEnvelope {
 	 * @return has z
 	 * @see #hasZ()
 	 */
-	public is3D (): boolean {
+	public is3D(): boolean {
 		return this.hasZ;
 	}
 	/**
@@ -101,7 +117,7 @@ export class GeometryEnvelope {
 	 * @return has m
 	 * @see #hasM()
 	 */
-	public isMeasured (): boolean {
+	public isMeasured(): boolean {
 		return this.hasM;
 	}
 
@@ -109,7 +125,7 @@ export class GeometryEnvelope {
 	 * Get min x
 	 * @return min x
 	 */
-	public get minX () {
+	public get minX() {
 		return this._minX;
 	}
 
@@ -117,7 +133,7 @@ export class GeometryEnvelope {
 	 * Set min x
 	 * @param minX min x
 	 */
-	public set minX (minX: number) {
+	public set minX(minX: number) {
 		this._minX = minX;
 	}
 
@@ -125,7 +141,7 @@ export class GeometryEnvelope {
 	 * Get max x
 	 * @return max x
 	 */
-	public get maxX (): number {
+	public get maxX(): number {
 		return this._maxX;
 	}
 
@@ -141,7 +157,7 @@ export class GeometryEnvelope {
 	 * Get min y
 	 * @return min y
 	 */
-	public get minY (): number {
+	public get minY(): number {
 		return this._minY;
 	}
 
@@ -149,7 +165,7 @@ export class GeometryEnvelope {
 	 * Set min y
 	 * @param minY min y
 	 */
-	public set minY (minY: number) {
+	public set minY(minY: number) {
 		this._minY = minY;
 	}
 
@@ -157,7 +173,7 @@ export class GeometryEnvelope {
 	 * Get max y
 	 * @return max y
 	 */
-	public get maxY () {
+	public get maxY() {
 		return this._maxY;
 	}
 
@@ -165,7 +181,7 @@ export class GeometryEnvelope {
 	 * Set max y
 	 * @param maxY max y
 	 */
-	public set maxY (maxY: number) {
+	public set maxY(maxY: number) {
 		this._maxY = maxY;
 	}
 
@@ -173,7 +189,7 @@ export class GeometryEnvelope {
 	 * True if has Z coordinates
 	 * @return has z
 	 */
-	public get hasZ (): boolean {
+	public get hasZ(): boolean {
 		return this._hasZ;
 	}
 
@@ -181,7 +197,7 @@ export class GeometryEnvelope {
 	 * Set has z coordinates
 	 * @param hasZ has z
 	 */
-	public set hasZ (hasZ: boolean) {
+	public set hasZ(hasZ: boolean) {
 		this._hasZ = hasZ;
 	}
 
@@ -189,7 +205,7 @@ export class GeometryEnvelope {
 	 * Get min z
 	 * @return min z
 	 */
-	public get minZ (): number {
+	public get minZ(): number {
 		return this._minZ;
 	}
 
@@ -197,7 +213,7 @@ export class GeometryEnvelope {
 	 * Set min z
 	 * @param minZ min z
 	 */
-	public set minZ (minZ: number) {
+	public set minZ(minZ: number) {
 		this._minZ = minZ;
 	}
 
@@ -205,7 +221,7 @@ export class GeometryEnvelope {
 	 * Get max z
 	 * @return max z
 	 */
-	public get maxZ (): number {
+	public get maxZ(): number {
 		return this._maxZ;
 	}
 
@@ -213,7 +229,7 @@ export class GeometryEnvelope {
 	 * Set max z
 	 * @param maxZ max z
 	 */
-	public set maxZ (maxZ: number) {
+	public set maxZ(maxZ: number) {
 		this._maxZ = maxZ;
 	}
 
@@ -221,7 +237,7 @@ export class GeometryEnvelope {
 	 * Has m coordinates
 	 * @return true if has m coordinates
 	 */
-	public get hasM (): boolean {
+	public get hasM(): boolean {
 		return this._hasM;
 	}
 
@@ -229,7 +245,7 @@ export class GeometryEnvelope {
 	 * Set has m coordinates
 	 * @param hasM has m
 	 */
-	public set hasM (hasM: boolean) {
+	public set hasM(hasM: boolean) {
 		this._hasM = hasM;
 	}
 
@@ -237,7 +253,7 @@ export class GeometryEnvelope {
 	 * Get min m
 	 * @return min m
 	 */
-	public get minM (): number {
+	public get minM(): number {
 		return this._minM;
 	}
 
@@ -253,7 +269,7 @@ export class GeometryEnvelope {
 	 * Get max m
 	 * @return max m
 	 */
-	public get maxM (): number {
+	public get maxM(): number {
 		return this._maxM;
 	}
 
@@ -277,7 +293,7 @@ export class GeometryEnvelope {
 	 * Get the y range
 	 * @return y range
 	 */
-	public get yRange (): number {
+	public get yRange(): number {
 		return this._maxY - this._minY;
 	}
 
@@ -285,7 +301,7 @@ export class GeometryEnvelope {
 	 * Get the z range
 	 * @return z range
 	 */
-	public get zRange (): number {
+	public get zRange(): number {
 		let range = null;
 		if (this._minZ != null && this._maxZ != null) {
 			range = this._maxZ - this._minZ;
@@ -310,16 +326,126 @@ export class GeometryEnvelope {
 	 * @return true if a single point bounds
 	 * @since 2.0.5
 	 */
-	public isPoint (): boolean {
+	public isPoint(): boolean {
 		return this._minX === this._maxX && this._minY === this._maxY
+	}
+
+	/**
+	 * Get the top left point
+	 * 
+	 * @return top left point
+	 * @since 1.0.3
+	 */
+	public getTopLeft(): Point {
+		return new Point(this.minX, this.maxY);
+	}
+
+	/**
+	 * Get the bottom left point
+	 * 
+	 * @return bottom left point
+	 * @since 1.0.3
+	 */
+	public getBottomLeft(): Point {
+		return new Point(this.minX, this.minY);
+	}
+
+	/**
+	 * Get the bottom right point
+	 * 
+	 * @return bottom right point
+	 * @since 1.0.3
+	 */
+	public getBottomRight(): Point {
+		return new Point(this.maxX, this.minY);
+	}
+
+	/**
+	 * Get the top right point
+	 * 
+	 * @return top right point
+	 * @since 1.0.3
+	 */
+	public getTopRight(): Point {
+		return new Point(this.maxX, this.maxY);
+	}
+
+	/**
+	 * Get the left line
+	 * 
+	 * @return left line
+	 * @since 1.0.3
+	 */
+	public getLeft(): Line {
+		return new Line(this.getTopLeft(), this.getBottomLeft());
+	}
+
+	/**
+	 * Get the bottom line
+	 * 
+	 * @return bottom line
+	 * @since 1.0.3
+	 */
+	public getBottom(): Line {
+		return new Line(this.getBottomLeft(), this.getBottomRight());
+	}
+
+	/**
+	 * Get the right line
+	 * 
+	 * @return right line
+	 * @since 1.0.3
+	 */
+	public getRight(): Line {
+		return new Line(this.getBottomRight(), this.getTopRight());
+	}
+
+	/**
+	 * Get the top line
+	 * 
+	 * @return top line
+	 * @since 1.0.3
+	 */
+	public getTop(): Line {
+		return new Line(this.getTopRight(), this.getTopLeft());
+	}
+
+	/**
+	 * Get the envelope mid x
+	 * 
+	 * @return mid x
+	 * @since 1.0.3
+	 */
+	public getMidX(): number {
+		return (this.minX + this.maxX) / 2.0;
+	}
+
+	/**
+	 * Get the envelope mid y
+	 * 
+	 * @return mid y
+	 * @since 1.0.3
+	 */
+	public getMidY(): number {
+		return (this.minY + this.maxY) / 2.0;
 	}
 
 	/**
 	 * Get the envelope centroid point
 	 * @return centroid point
 	 */
-	public get centroid (): Point {
+	public get centroid(): Point {
 		return new Point((this._minX + this._maxX) / 2.0, (this._minY + this._maxY) / 2.0);
+	}
+
+	/**
+	 * Determine if the envelope is empty
+	 * 
+	 * @return true if empty
+	 * @since 1.0.3
+	 */
+	public isEmpty(): boolean {
+		return this.xRange <= 0.0 || this.yRange <= 0.0;
 	}
 
 	/**
@@ -328,7 +454,7 @@ export class GeometryEnvelope {
 	 * @param allowEmpty allow empty ranges when determining intersection
 	 * @return true if intersects
 	 */
-	public intersects (envelope: GeometryEnvelope, allowEmpty: boolean): boolean {
+	public intersects(envelope: GeometryEnvelope, allowEmpty: boolean): boolean {
 		return this.overlap(envelope, allowEmpty) != null;
 	}
 
@@ -338,7 +464,7 @@ export class GeometryEnvelope {
 	 * @param allowEmpty allow empty ranges when determining intersection
 	 * @return geometry envelope
 	 */
-	public overlap (envelope: GeometryEnvelope, allowEmpty: boolean = false): GeometryEnvelope {
+	public overlap(envelope: GeometryEnvelope, allowEmpty: boolean = false): GeometryEnvelope {
 		const minX = Math.max(this.minX, envelope.minX);
 		const maxX = Math.min(this.maxX, envelope.maxX);
 		const minY = Math.max(this.minY, envelope.minY);
@@ -358,7 +484,7 @@ export class GeometryEnvelope {
 	 * @param envelope geometry envelope
 	 * @return geometry envelope
 	 */
-	public union (envelope: GeometryEnvelope): GeometryEnvelope {
+	public union(envelope: GeometryEnvelope): GeometryEnvelope {
 		const minX = Math.max(this.minX, envelope.minX);
 		const maxX = Math.min(this.maxX, envelope.maxX);
 		const minY = Math.max(this.minY, envelope.minY);
@@ -374,22 +500,93 @@ export class GeometryEnvelope {
 	}
 
 	/**
+	 * Determine if contains the point
+	 *
+	 * @param point
+	 *            point
+	 * @return true if contains
+	 * @since 1.0.3
+	 */
+	public containsPoint(point: Point): boolean {
+		return this.containsPointWithEpsilon(point, 0.0);
+	}
+
+	/**
+	 * Determine if contains the point
+	 *
+	 * @param point
+	 *            point
+	 * @param epsilon
+	 *            epsilon equality tolerance
+	 * @return true if contains
+	 * @since 1.0.3
+	 */
+	public containsPointWithEpsilon(point: Point, epsilon: number): boolean {
+		return this.containsCoordsWithEpsilon(point.x, point.y, epsilon);
+	}
+
+	/**
+	 * Determine if contains the coordinate
+	 *
+	 * @param x
+	 *            x value
+	 * @param y
+	 *            y value
+	 * @return true if contains
+	 * @since 1.0.3
+	 */
+	public containsCoords(x: number, y: number): boolean {
+		return this.containsCoordsWithEpsilon(x, y, 0.0);
+	}
+
+	/**
+	 * Determine if contains the coordinate
+	 *
+	 * @param x
+	 *            x value
+	 * @param y
+	 *            y value
+	 * @param epsilon
+	 *            epsilon equality tolerance
+	 * @return true if contains
+	 * @since 1.0.3
+	 */
+	public containsCoordsWithEpsilon(x: number, y: number, epsilon: number): boolean {
+		return x >= this.minX - epsilon && x <= this.maxX + epsilon
+			&& y >= this.minY - epsilon && y <= this.maxY + epsilon;
+	}
+
+	/**
 	 * Determine if inclusively contains the provided envelope
 	 * @param envelope geometry envelope
 	 * @return true if contains
 	 */
 	public contains(envelope: GeometryEnvelope): boolean {
-		return this.minX <= envelope.minX
-				&& this.maxX >= envelope.maxX
-				&& this.minY <= envelope.minY
-				&& this.maxY >= envelope.maxY;
+		return this.containsWithEpsilon(envelope, 0.0);
+	}
+
+	/**
+	 * Determine if inclusively contains the provided envelope
+	 *
+	 * @param envelope
+	 *            geometry envelope
+	 * @param epsilon
+	 *            epsilon equality tolerance
+	 * @return true if contains
+	 * @since 1.0.3
+	 */
+	public containsWithEpsilon(envelope: GeometryEnvelope, epsilon: number): boolean {
+		return this.minX - epsilon <= envelope.minX
+			&& this.maxX + epsilon >= envelope.maxX
+			&& this.minY - epsilon <= envelope.minY
+			&& this.maxY + epsilon >= envelope.maxY;
 	}
 
 	/**
 	 * Build a geometry representation of the geometry envelope
 	 * @return geometry, polygon or point
 	 */
-	public buildGeometry (): Geometry {
+	public buildGeometry(): Geometry {
 		let geometry: Geometry;
 		if (this.isPoint()) {
 			geometry = new Point(this.minX, this.minY);
@@ -410,7 +607,7 @@ export class GeometryEnvelope {
 	 * Copy the geometry envelope
 	 * @return geometry envelope copy
 	 */
-	public copy (): GeometryEnvelope {
+	public copy(): GeometryEnvelope {
 		return new GeometryEnvelope(this);
 	}
 
